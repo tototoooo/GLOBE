99d95dfe152a2595287a43803419d356d776115a
tototoooo <diarietou.sy@ensea.fr>
2022-01-21 08:31:04 +0100

    change

---
+++ src/MAIN.java
@@ -0,0 +1,25 @@
+public class MAIN {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Airports aero1 = new Airports("DSU","IOTA", "FR",265, 340);
+        Airports aero2 = new Airports("DSA","NUA", "ESPN",255, 300);
+
+
+
+        System.out.println(aero1.toString());
+        System.out.println(aero2.toString());
+        //System.out.println("Nombre d'aréoports : " +aero1.getNumber());
+        //etu2 = null;
+        World w = new World ("./data/airport-codes_no_comma.csv");
+        System.out.println("Found "+w.getaeroList().size()+" airports.");
+        Airports paris = w.findNearestAirport(2.316,48.866);
+        Airports cdg = w.findByCode("CDG");
+        double distance = w.distance(2.316,48.866,paris.getLongitude(),paris.getLatitude());
+        System.out.println(paris);
+        System.out.println(distance);
+        double distanceCDG = w.distance(2.316,48.866,cdg.getLongitude(),cdg.getLatitude());
+        System.out.println(cdg);
+        System.out.println(distanceCDG);
+    }
+}
---
+++ src/World.java
@@ -0,0 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+import static java.lang.Math.*;
+
+public class World {
+    ArrayList<Airports> aeroList;
+    public ArrayList<Airports> getaeroList() {
+        return aeroList;
+    }
+
+
+    public World(String fileName) {
+        ArrayList<Airports> listAirports = new ArrayList<Airports>();
+        try {
+            BufferedReader buf = new BufferedReader(new FileReader(fileName));
+            String s = buf.readLine();
+            while (s != null) {
+                s = s.replaceAll("\"", "");
+//Enleve les guillemets qui s´eparent les champs de donn´ees GPS.
+                String fields[] = s.split(",");
+// Une bonne id´ee : placer un point d'arr^et ici pour du debuggage.
+                if (fields[1].equals("large_airport")) {
+
+                    Airports aero1= new Airports( fields[9], fields[2], fields[5], Double.parseDouble(fields[11]),Double.parseDouble(fields[12]));
+                }
+                s = buf.readLine();
+            }
+        }
+
+        catch(Exception e){
+            System.out.println("Maybe the file isn't there ?");
+            System.out.println(aeroList.get(aeroList.size() - 1));
+            e.printStackTrace();
+        }
+
+    }
+    public Airports findByCode(String code){
+        for( Airports aero : aeroList){
+            if(aero.getIATA().equals(code)){
+                return aero;
+            }
+        }
+        System.out.println("Le code IATA ne correspond à aucun aeroport");
+        return null;
+    }
+
+    public Airports findNearestAirport(double lon, double lat){
+        double dist = distance(lon,lat,aeroList.get(0).getLongitude(),aeroList.get(0).getLatitude());
+        Airports b = null;
+        double index = 0;
+        for(Airports a : aeroList){
+            double lat1 = a.getLatitude();
+            double lon1 = a.getLongitude();
+            double d = distance(lon,lat,lon1, lat1);
+            if(d < dist){
+                b = a;
+                dist = d;
+            }
+        }
+        return b;
+    }
+
+    public double distance(double lo1, double la1, double lo2, double la2){
+        double norme = pow(la2 - la1, 2) + pow((lo2 - lo1) * cos(toRadians((la2 + la1) / 2)), 2);
+        return norme;
+    }
+
+
+}
+
